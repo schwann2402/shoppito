04f07513e626529a782539f51088f503299d207a
schwann2402 <139656534+schwann2402@users.noreply.github.com>
2024-11-08 15:08:51 -0500

    Created hero component of homepage frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Hero from "../components/Hero";
+
 const Home = () => {
-  return <div className="min-h-[200px]">Home</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
